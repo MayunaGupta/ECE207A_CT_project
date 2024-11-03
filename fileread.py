a54dc656b97332f227f1ef2233ee75609ffadc90
mayunag <mayuna.gupta@paraxialtech.com>
2024-11-02 19:13:08 -0700

    Temporal Scans

--- fileread.py
+++ fileread.py
@@ -3,21 +3,23 @@ import numpy as np
 import os
 
 print("Reading Dicom directory:", )
-reader = sitk.ImageSeriesReader()
+# reader = sitk.ImageSeriesReader()
 
-dicom_names = reader.GetGDCMSeriesFileNames("/Users/mayunagupta/Downloads/Non-contrast Cardiac CT Images Dataset with Coronary Artery Calcium Scoring/Dataset/Patient/Patient_18/SE0001")
-reader.SetFileNames(dicom_names)
+# dicom_names = reader.GetGDCMSeriesFileNames("/Users/mayunagupta/Downloads/Non-contrast Cardiac CT Images Dataset with Coronary Artery Calcium Scoring/Dataset/Patient/Patient_18/SE0001")
+# reader.SetFileNames(dicom_names)
 
-print(dicom_names)
+# print(dicom_names)
 
-image = reader.Execute()
-
+# image = reader.Execute()
+image = sitk.ReadImage("test.nii.gz")
 size = image.GetSize()
 print("Image size:", size[0], size[1], size[2])
 
 # print("Writing image:", sys.argv[2])
 
-sitk.WriteImage(image, "test.nii.gz")
+# sitk.WriteImage(image, "test.nii.gz")
+
+
 
 # if "SITK_NOSHOW" not in os.environ:
 #     sitk.Show(image, "Dicom Series")
